4c3dcb343bfe0dc28f6b324132e106d5ce69726b
ghazi-code <ghazi.khiari@edu.esiee-it.fr>
2026-03-13 16:16:17 +0100

    add enemy manager

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -6,16 +7,20 @@ public class Spawner : MonoBehaviour
     [SerializeField] public Transform target;
     [SerializeField] GameObject Enemy;
     [SerializeField] float Seconds;
+    [SerializeField] int numberOfEnemy;
+    [SerializeField] int pvMultiplicator;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     IEnumerator Start()
     {
-        while (true)
-        {
+        for (int i = 0; i < numberOfEnemy; i++){
             yield return new WaitForSeconds(Seconds);
             GameObject enemy = Instantiate(Enemy, transform);
+
             enemy.GetComponent<EnemyMovement>().target = target;
+            enemy.GetComponent<Health>().HP *= pvMultiplicator;
+            
         }
     }
 
