90d820ab8d69e7a7c7d7fcf654146e0d85653d3d
ghazi-code <ghazi.khiari@edu.esiee-it.fr>
2026-03-13 14:32:35 +0100

    add drag and drop snap

--- Assets/Scripts/DragDrop2D.cs
+++ Assets/Scripts/DragDrop2D.cs
@@ -1,19 +1,43 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragDrop2D : MonoBehaviour, IBeginDragHandler, IDragHandler
+public class DragDrop2D : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private Vector3 offset;
+    Collider2D col;
+    public string destinationTag = "DropZone";
+
+    private void Awake()
+    {
+        col = GetComponent<Collider2D>();
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("Début du drag !");
         offset = transform.position - GetWorldPosition(eventData.position);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         transform.position = GetWorldPosition(eventData.position) + offset;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        col.enabled = false;
+
+        Vector3 worldPointerPos = GetWorldPosition(eventData.position);
+        RaycastHit2D hitInfo = Physics2D.Raycast(worldPointerPos, Vector2.zero);
+
+        if (hitInfo.collider != null)
+        {
+            if (hitInfo.transform.tag == destinationTag)
+            {
+                transform.position = hitInfo.transform.position + new Vector3(0, 0, -0.01f);
+            }
+        }
+
+        col.enabled = true;
     }
 
     Vector3 GetWorldPosition(Vector2 screenPoint)
