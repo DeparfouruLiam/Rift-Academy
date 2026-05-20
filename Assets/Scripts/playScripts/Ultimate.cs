7ffdc3044f403d5375e9d71fd9782b3e8aa290d8
ghazi-code <ghazi.khiari@edu.esiee-it.fr>
2026-05-20 08:36:39 +0200

    add timer ultimate

--- Assets/Scripts/playScripts/Ultimate.cs
+++ Assets/Scripts/playScripts/Ultimate.cs
@@ -1,13 +1,35 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System.Collections; 
 
 public class Ultimate : MonoBehaviour, IPointerClickHandler
 {
-    [SerializeField] GameObject hero;
-    public void OnPointerClick(PointerEventData eventData) {
-        Debug.Log("gooning");
-        hero.GetComponent<HeroAttack>().ultimateUp = true;
+    [SerializeField] private GameObject hero;
+    [SerializeField] private float ultimateDuration = 5f; 
+
+    private bool isUltimateActive = false;
+
+    public void OnPointerClick(PointerEventData eventData) 
+    {
+        if (!isUltimateActive) 
+        {
+            Debug.Log("Ultimate activé !");
+            StartCoroutine(UltimateTimerRoutine());
+        }
     }
-    
+
+    private IEnumerator UltimateTimerRoutine() 
+    {
+        isUltimateActive = true;
+        HeroAttack heroAttack = hero.GetComponent<HeroAttack>();
+
+        if (heroAttack != null) 
+        {
+            heroAttack.ultimateUp = true;
+            yield return new WaitForSeconds(ultimateDuration);
+            heroAttack.ultimateUp = false;
+        }
+        
+        isUltimateActive = false;
+    }
 }
