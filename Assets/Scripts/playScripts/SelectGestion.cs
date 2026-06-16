5c914695b0abcbb7e84062024838fe6b7e38302e
ghazi-code <ghazi.khiari@edu.esiee-it.fr>
2026-06-16 10:47:16 +0200

    final select heros

--- Assets/Scripts/playScripts/SelectGestion.cs
+++ Assets/Scripts/playScripts/SelectGestion.cs
@@ -1,34 +1,37 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class SelectGestion : MonoBehaviour
 {
     public SlotHero[] slotsEquipe;
 
-    public void SelectionnerHero(Button personnageClique)
+    public void SelectionnerHero() 
     {
+        GameObject objetClique = EventSystem.current.currentSelectedGameObject;
+        
+        if (objetClique == null) return;
+
+        Button personnageClique = objetClique.GetComponent<Button>();
+        if (personnageClique == null) return;
+
         for (int i = 0; i < slotsEquipe.Length; i++)
         {
-            
             if (slotsEquipe[i].estOccupe == false)
             {
                 Image imageDuPersoClique = personnageClique.GetComponent<Image>();
-              
                 Image imageDuSlot = slotsEquipe[i].GetComponent<Image>();
-               
+
                 imageDuSlot.sprite = imageDuPersoClique.sprite;
-
                 imageDuSlot.color = Color.white;
-
                 slotsEquipe[i].estOccupe = true;
 
                 personnageClique.interactable = false;
-
                 imageDuPersoClique.color = new Color(1f, 1f, 1f, 0.3f);
+                
                 return;
             }
         }
-
-        Debug.Log("Équipe pleine ou action impossible !");
+        Debug.Log("Équipe pleine !");
     }
 }
